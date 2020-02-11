5eea85fbd9272d3a33250c6184bf8e8c64440a5b
akarnachev <a.karnachev@just-ai.com>
2020-02-11 23:07:01 +0300

    refactoring finished. waveglow can be trained now

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -47,7 +47,7 @@ if __name__ == '__main__':
     dl = mel2samp.prepare_dataloaders(hparams)
 
     model = factory.Factory.get_class(f'waveglow.models.{hparams.model_class_name}')(hparams)
-    optimizer = AdamW(model.parameters(), lr=hparams.learning_rate, weight_decay=hparams.weight_decay)
+    optimizer = AdamW(model.parameters(), lr=hparams.learning_rate, weight_decay=hparams.weight_decay or 0)
 
     summary_writer = SummaryWriter(log_dir=tb_logdir)
 
