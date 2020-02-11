dc11793b596496ba4b09462025595bbdf126c944
akarnachev <a.karnachev@just-ai.com>
2020-02-11 23:28:07 +0300

    TensorBoardLoggerCallback was added

--- waveglow/callbacks/callbacks.py
+++ waveglow/callbacks/callbacks.py
@@ -1,4 +1,4 @@
-from tacotron2 import learner
+from tacotron2 import learner as taco_learner
 from tacotron2.callbacks import _callback as callback
 from torch.utils import tensorboard
 
@@ -13,5 +13,24 @@ class TensorBoardLoggerCallback(callback.Callback):
         """
         self.summary_writer = summary_writer
 
-    def on_opt_step(self, learner: learner.Learner):
-        pass
+    def on_opt_step(self, learner: taco_learner.Learner):
+        """Writes training loss, grad. norm and learning rate at each optimizer step.
+
+        Args:
+            learner: tacotron2 learner object.
+
+        """
+        learning_rate = learner.optimizer.param_groups[0]['lr']
+
+        self.summary_writer.add_scalar("training.loss", learner.train_loss, learner.overall_step)
+        self.summary_writer.add_scalar("grad.norm", learner.grad_norm, learner.overall_step)
+        self.summary_writer.add_scalar("learning.rate", learning_rate, learner.overall_step)
+
+    def on_eval_end(self, learner: taco_learner.Learner):
+        """Writes validation loss at each validation step end.
+
+        Args:
+            learner: tacotron2 learner object
+
+        """
+        self.summary_writer.add_scalar("validation.loss", learner.valid_loss, learner.overall_step)
