5b81b2ca528c24230a9146a6eec5a463664f139b
Egor Timofeev <timofeev.egor.perm@gmail.com>
2020-08-10 14:56:34 +0300

    Remove unused requirements, torch 1.4.0 -> 1.5.0

--- setup.py
+++ setup.py
@@ -18,14 +18,11 @@ setup(
     description='Waveglow vocoder model',
     packages=find_packages(exclude=['test', 'test.*']),
     install_requires=[
-        "torch==1.4.0",
-        "scikit-learn==0.21.3",
-        "pandas==0.25.1",
+        "torch==1.5.0",
         "future==0.17.1",
         "tqdm==4.36.1",
         "librosa==0.7.2",
         "pillow==7.0.0",
-        "matplotlib==3.1.3",
         "transformers==2.4.1",
     ],
     package_dir={'waveglow': 'waveglow'}
